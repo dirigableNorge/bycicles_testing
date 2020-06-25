8379f26fee7ff0e2418ad5bdef360dc5518f74bb
tarasyi <dirigablenorge@gmail.com>
2020-06-25 16:53:20 +0400

    fix bugs

--- source/js/script.js
+++ source/js/script.js
@@ -7,17 +7,26 @@ if (pageHeader) {
   pageHeader.classList.add('nav--closed');
 }
 
-if (headerToggle) {
-  headerToggle.addEventListener('click', function () {
-    if (pageHeader.classList.contains('nav--closed')) {
-      pageHeader.classList.remove('nav--closed');
-      pageHeader.classList.add('nav--opened');
-    } else {
-      pageHeader.classList.add('nav--closed');
-      pageHeader.classList.remove('nav--opened');
-    }
-  });
+function navToggle () {
+  if (pageHeader.classList.contains('nav--closed')) {
+    pageHeader.classList.remove('nav--closed');
+    pageHeader.classList.add('nav--opened');
+  } else {
+    pageHeader.classList.add('nav--closed');
+    pageHeader.classList.remove('nav--opened');
+  }
 }
+
+if (headerToggle) {
+  headerToggle.addEventListener('click', navToggle);
+}
+
+var navLinks = document.querySelectorAll('.nav__link');
+
+navLinks.forEach(function (link) {
+  link.addEventListener('click', navToggle);
+});
+
 
 var smoothScroll = new SmoothScroll('a[href^="#"]');
 
